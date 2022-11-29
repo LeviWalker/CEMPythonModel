ccf12104b2597c75e8d935c6ca2ad320fc3cef22
LeviWalker <levi.j.walker04@gmail.com>
2022-11-28 19:57:12 -0500

    constructors for level 0 and subclasses

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from dataclasses import dataclass
 
 
-def readCSV(filename: str):
+def readFile(filename: str):
     with open(filename, mode='r') as file:
         data = csv.reader(file)
 
@@ -11,12 +11,11 @@ def readCSV(filename: str):
             print(line)
 
 
-readCSV("UAM Test Data.csv")
+readFile("UAM Test Data.csv")
 
 
-@dataclass
 class Level0HardwareGivens:
-    range: float
+    vehicle_range: float
     max_speed: float
     energy_cap: float
     energy_consumption_dist: float
@@ -29,8 +28,24 @@ class Level0HardwareGivens:
     vehicle_base_weight: float
     max_total_passenger_weight: float
 
+    def __init__(self, vehicle_range: float, max_speed: float, energy_cap: float, energy_consumption_dist: float,
+                 num_passengers: float, max_luggage_weight: float, vehicle_base_cost: float, ingress_egress_time: float,
+                 landing_takeoff_time: float, bad_event_probability: float, vehicle_base_weight: float,
+                 max_total_passenger_weight: float):
+        self.vehicle_range = vehicle_range
+        self.max_speed = max_speed
+        self.energy_cap = energy_cap
+        self.energy_consumption_dist = energy_consumption_dist
+        self.num_passengers = num_passengers
+        self.max_luggage_weight = max_luggage_weight
+        self.vehicle_base_cost = vehicle_base_cost
+        self.ingress_egress_time = ingress_egress_time
+        self.landing_takeoff_time = landing_takeoff_time
+        self.bad_event_probability = bad_event_probability
+        self.vehicle_base_weight = vehicle_base_weight
+        self.max_total_passenger_weight = max_total_passenger_weight
 
-@dataclass
+
 class Level0GeographicGivens:
     energy_cost: float
     airspace_classification: str
@@ -44,8 +59,23 @@ class Level0GeographicGivens:
     living_cost: float
     num_emergency_landing_sites: float
 
+    def __init__(self, energy_cost: float, airspace_classification: str, max_charging_refuel_rate: float,
+                 trip_dist: float, land_cost: float, building_cost: float, av_taxi_cost: float,
+                 av_rideshare_cost: float, median_household_income: float, living_cost: float,
+                 num_emergency_landing_sites: float):
+        self.energy_cost = energy_cost
+        self.airspace_classification = airspace_classification
+        self.max_charging_refuel_rate = max_charging_refuel_rate
+        self.trip_dist = trip_dist
+        self.land_cost = land_cost
+        self.building_cost = building_cost
+        self.av_taxi_cost = av_taxi_cost
+        self.av_rideshare_cost = av_rideshare_cost
+        self.median_household_income = median_household_income
+        self.living_cost = living_cost
+        self.num_emergency_landing_sites = num_emergency_landing_sites
 
-@dataclass
+
 class Level0UserGivens:
     total_trip_time: float
     max_tolerable_trip_cost: float
@@ -53,13 +83,27 @@ class Level0UserGivens:
     max_possible_luggage_per_passenger_weight: float
     num_trip_passengers: float
 
+    def __init__(self, total_trip_time: float, max_tolerable_trip_cost: float, max_tolerable_ingress_egress_time: float,
+                 max_possible_luggage_per_passenger_weight: float, num_trip_passengers: float):
+        self.total_trip_time = total_trip_time
+        self.max_tolerable_trip_cost = max_tolerable_trip_cost
+        self.max_tolerable_ingress_egress_time = max_tolerable_ingress_egress_time
+        self.max_possible_luggage_per_passenger_weight = max_possible_luggage_per_passenger_weight
+        self.num_trip_passengers = num_trip_passengers
 
-@dataclass
+
 class Level0EconomicGivens:
     corp_overhead_costs: float
     general_target_price_per_passenger_ride: float
     flight_controller_employment_cost: float
     break_even_point: float
+
+    def __init__(self, corp_overhead_costs: float, general_target_price_per_passenger_ride: float,
+                 flight_controller_employment_cost: float, break_even_point: float):
+        self.corp_overhead_costs = corp_overhead_costs
+        self.general_target_price_per_passenger_ride = general_target_price_per_passenger_ride
+        self.flight_controller_employment_cost = flight_controller_employment_cost
+        self.break_even_point = break_even_point
 
 
 @dataclass
@@ -68,6 +112,13 @@ class Level0:
     geography: Level0GeographicGivens
     user: Level0UserGivens
     economy: Level0EconomicGivens
+
+    def __init__(self, hardware: Level0HardwareGivens, geography: Level0GeographicGivens, user: Level0UserGivens,
+                 economy: Level0EconomicGivens):
+        self.hardware = hardware
+        self.geography = geography
+        self.user = user
+        self.economy = economy
 
 
 class Level1:
